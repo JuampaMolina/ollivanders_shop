af3b56e374fbfa8957a876f3ec2ec7453a61fdd4
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-18 12:01:23 +0100

    test(test_db): adapt test for new data structure

--- test/test_db.py
+++ test/test_db.py
@@ -5,4 +5,13 @@ from repository.db import DB
 
 @pytest.mark.db_get_item
 def test_get_item():
-    assert DB.get_item("Aged Brie") == [["Aged Brie", 2, 0]]
+    assert DB.get_item("Aged Brie") == [{
+            "name": "Aged Brie",
+            "sell_in": 2,
+            "quality": 0
+        },
+        {
+            "name": "Aged Brie",
+            "sell_in": 5,
+            "quality": 2
+        }]

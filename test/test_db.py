ce2cc08c1e4815f0df541e576613aa597aee6951
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-18 11:48:47 +0100

    fix(test_db): fix imports

--- test/test_db.py
+++ test/test_db.py
@@ -1,8 +1,8 @@
 import pytest
 
-from repository import db
+from repository.db import DB
 
 
 @pytest.mark.db_get_item
 def test_get_item():
-    assert db.get_item("Aged Brie") == ["Aged Brie", 2, 0]
+    assert DB.get_item("Aged Brie") == [["Aged Brie", 2, 0]]

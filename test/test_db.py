b112e080e98d926d790c1c62f7545fedcfb1a77b
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-22 17:12:49 +0100

    test(get_inventory): added get_inventory test

--- test/test_db.py
+++ test/test_db.py
@@ -1,12 +1,11 @@
 import json
 import pytest
+from repository.db import DB
 
 
 @pytest.mark.db_get_item
 def test_get_item(client):
     rv = client.get('/item/Aged Brie')
-    # assert b'[{"_id": {"$oid": "605890fbdf7ed8ef465527ee"},"name": "Aged Brie","sell_in": 2,"quality": 0}]' in rv.data
-    # assert DB.get_item("Aged Brie") == [{'_id': {'$oid': '605890fbdf7ed8ef465527ee'}, 'name': 'Aged Brie', 'quality': 0, 'sell_in': 2}]
     assert json.loads(rv.data) == [
         {
             "_id": {
@@ -17,3 +16,82 @@ def test_get_item(client):
             "quality": 0
         }
     ]
+
+@pytest.mark.db_get_inventory
+def test_get_all_items(client):
+    DB.load_database()
+    rv = client.get('/inventory')
+    assert json.loads(rv.data) == [
+    {
+        "_id": {
+            "$oid": "605890fbdf7ed8ef465527ee"
+        },
+        "name": "Aged Brie",
+        "sell_in": 2,
+        "quality": 0
+    },
+    {
+        "_id": {
+            "$oid": "6058c0385a343a36b273fd71"
+        },
+        "name": "+5 Dexterity Vest",
+        "sell_in": 10,
+        "quality": 20
+    },
+    {
+        "_id": {
+            "$oid": "6058c0395a343a36b273fd72"
+        },
+        "name": "Elixir of the Mongoose",
+        "sell_in": 5,
+        "quality": 7
+    },
+    {
+        "_id": {
+            "$oid": "6058c0395a343a36b273fd73"
+        },
+        "name": "Sulfuras, Hand of Ragnaros",
+        "sell_in": 0,
+        "quality": 80
+    },
+    {
+        "_id": {
+            "$oid": "6058c0395a343a36b273fd74"
+        },
+        "name": "Sulfuras, Hand of Ragnaros",
+        "sell_in": -1,
+        "quality": 80
+    },
+    {
+        "_id": {
+            "$oid": "6058c0395a343a36b273fd75"
+        },
+        "name": "Backstage passes to a TAFKAL80ETC concert",
+        "sell_in": 15,
+        "quality": 20
+    },
+    {
+        "_id": {
+            "$oid": "6058c0395a343a36b273fd76"
+        },
+        "name": "Backstage passes to a TAFKAL80ETC concert",
+        "sell_in": 10,
+        "quality": 49
+    },
+    {
+        "_id": {
+            "$oid": "6058c0395a343a36b273fd77"
+        },
+        "name": "Backstage passes to a TAFKAL80ETC concert",
+        "sell_in": 5,
+        "quality": 49
+    },
+    {
+        "_id": {
+            "$oid": "6058c0395a343a36b273fd78"
+        },
+        "name": "Conjured Mana Cake",
+        "sell_in": 3,
+        "quality": 6
+    }
+]

33a857c997e17a993fe44ae41480fcad648c04cc
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-23 01:01:02 +0100

    feat(app): '/home' endpoint for see the inventory in html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
-
+import json
 from controller.inventory import Inventory
 from controller.items import Items
+from repository.db import DB
+from services.service import Service
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,6 +13,12 @@ api = Api(app)
 class WelcomeOllivanders(Resource):
     def get(self):
         return {'Welcome': 'Ollivanders'}
+
+
+@app.route('/home')
+def show_inventory():
+    items = json.loads(Service.get_all_items().data)
+    return render_template('inventory.html', inventory=items)
 
 
 api.add_resource(WelcomeOllivanders, '/')

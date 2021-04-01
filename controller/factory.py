cfc497a77d144d8b50746d2ae16426e17fb601f3
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-01 11:01:51 +0200

    test(): rebuilded tests

--- controller/factory.py
+++ controller/factory.py
@@ -1,21 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
+from flask_cors import CORS
 from controller.inventory import Inventory
 from controller.items import Items
+from controller.quality import Quality
+from controller.sell_in import Sell_in
+from repository.db_engine import init_app
 
 
 def create_app():
-    app = Flask(__name__)
 
+    app = Flask(__name__)
+    CORS(app)
     api = Api(app)
+    init_app(app)
 
     class WelcomeOllivanders(Resource):
         def get(self):
             return {"Welcome": "Ollivanders"}
 
     api.add_resource(WelcomeOllivanders, "/")
-    api.add_resource(Items, "/item/<name>", "/item/delete/<id>", "/item/add")
     api.add_resource(Inventory, "/inventory")
+    api.add_resource(Items, "/item/<name>", "/item/delete/<id>", "/item/add")
+    api.add_resource(Quality, "/item/quality/<int:quality>")
+    api.add_resource(Sell_in, "/item/sell_in/<int:sell_in>")
 
     return app
--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-
 from controller.factory import create_app
 
 

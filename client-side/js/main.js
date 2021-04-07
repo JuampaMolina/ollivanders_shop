8e54d2469518d20319f8c767c2952102427700fa
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-07 11:02:09 +0200

    feat(client-side): added update quality to the client side with buttons

--- client-side/js/main.js
+++ client-side/js/main.js
@@ -1,11 +1,31 @@
 const section = document.querySelector('.parent');
 
 function loadItems() {
+    removeCards();
     fetch('http://127.0.0.1:5000/inventory')
         .then(response => response.json()) //response to JSON
         .then(data => {
             data.forEach(doc => {
-                const card = document.createElement('div');
+                makeCards(doc);
+            });
+        })
+        .catch(error => console.log('It was an error: ' + error.message))
+}
+
+function updateQuality() {
+    removeCards();
+    fetch('http://127.0.0.1:5000/update_quality')
+        .then(response => response.json()) //response to JSON
+        .then(data => {
+            data.forEach(doc => {
+                makeCards(doc);
+            });
+        })
+        .catch(error => console.log('It was an error: ' + error.message))
+}
+
+function makeCards(doc){
+    const card = document.createElement('div');
                 card.className = 'card'
                 card.innerHTML += `
                     <h3>${doc.name}</h3>
@@ -13,9 +33,9 @@ function loadItems() {
                     <h4>Quality: ${doc.quality}</h4>
                 `;
                 section.appendChild(card);
-            });
-        })
-        .catch(error => console.log('It was an error: ' + error.message))
 }
 
-loadItems();
+function removeCards() {
+    var e = document.querySelector(".parent");
+        e.innerHTML = "";
+}

97424b20d50a5cf076de16c5038d88a47b9029f5
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-07 13:27:46 +0200

    test: used default_inventory from SetupTestDB class

--- test/test_db.py
+++ test/test_db.py
@@ -1,37 +1,17 @@
 import json
-
+from test.conftest import SetupTestDB
 import pytest
+
+expectedInventory = SetupTestDB.default_inventory
 
 
 @pytest.mark.db_get_item_by_name
 def test_get_item_by_name(client):
-    rv = client.get("/item/Aged Brie")
+    rv = client.get("/item/name/Aged Brie")
     assert json.loads(rv.data) == [{"name": "Aged Brie", "sell_in": 2, "quality": 0}]
 
 
 @pytest.mark.db_get_inventory
 def test_get_all_items(client):
     rv = client.get("/inventory")
-    assert json.loads(rv.data) == [
-        {"name": "Aged Brie", "sell_in": 2, "quality": 0},
-        {"name": "+5 Dexterity Vest", "sell_in": 10, "quality": 20},
-        {"name": "Elixir of the Mongoose", "sell_in": 5, "quality": 7},
-        {"name": "Sulfuras, Hand of Ragnaros", "sell_in": 0, "quality": 80},
-        {"name": "Sulfuras, Hand of Ragnaros", "sell_in": -1, "quality": 80},
-        {
-            "name": "Backstage passes to a TAFKAL80ETC concert",
-            "sell_in": 15,
-            "quality": 20,
-        },
-        {
-            "name": "Backstage passes to a TAFKAL80ETC concert",
-            "sell_in": 10,
-            "quality": 49,
-        },
-        {
-            "name": "Backstage passes to a TAFKAL80ETC concert",
-            "sell_in": 5,
-            "quality": 49,
-        },
-        {"name": "Conjured Mana Cake", "sell_in": 3, "quality": 6},
-    ]
+    assert json.loads(rv.data) == expectedInventory

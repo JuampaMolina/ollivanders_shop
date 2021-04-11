ded33f4d414d4b1a4596cc33acdf5212004ac5b7
JuampaMolina <jruiz@cifpfbmoll.eu>
2021-04-11 18:44:57 +0200

    feat(client-side): register user from front

---
+++ client-side/js/registerUser.js
@@ -0,0 +1,27 @@
+let formRegisterUser = document.querySelector('.register-user');
+formRegisterUser.register.addEventListener('click', registerUser);
+
+function registerUser(e) {
+    e.preventDefault();
+
+    let data = {
+        user_name: formRegisterUser.elements.name.value,
+        email: formRegisterUser.elements.email.value,
+        password: formRegisterUser.elements.password.value
+    };
+
+    fetch('http://0.0.0.0:4000/user', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json()) 
+        .then(data => {
+                alert(data.message)
+            }
+        )
+        .catch(error => console.log('It was an error: ' + error.message))
+
+}

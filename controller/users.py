16720f3da87f2bec500057a03b7f46416fbba40d
JuampaMolina <jruiz@cifpfbmoll.eu>
2021-04-11 18:50:13 +0200

    refactor(db): automatize credit and empty inventory when register

--- controller/users.py
+++ controller/users.py
@@ -14,15 +14,7 @@ class Users(Resource):
     @staticmethod
     def parseRequest():
         parser = reqparse.RequestParser(bundle_errors=True)
-        parser.add_argument(
-            "user_name", type=str, required=True, help="user_name required"
-        )
+        parser.add_argument("user_name", type=str, required=True, help="user_name required")
         parser.add_argument("email", type=str, required=True, help="email required")
-        parser.add_argument(
-            "password", type=str, required=True, help="password required"
-        )
-        parser.add_argument("credit", type=int)
-        parser.add_argument(
-            "inventory", type=dict, action="append", required=False
-        )  # action append ojo
+        parser.add_argument("password", type=str, required=True, help="password required")
         return parser.parse_args()

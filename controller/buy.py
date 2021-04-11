f6c079a7b3cdedb90a9836bab758a57ad5c2f96d
JuampaMolina <jruiz@cifpfbmoll.eu>
2021-04-11 18:50:51 +0200

    refactor(db): add password and item properties to buy

--- controller/buy.py
+++ controller/buy.py
@@ -11,8 +11,9 @@ class Buy(Resource):
     @staticmethod
     def parseRequest():
         parser = reqparse.RequestParser(bundle_errors=True)
-        parser.add_argument(
-            "user_name", type=str, required=True, help="user_name required"
-        )
+        parser.add_argument("user_name", type=str, required=True, help="user_name required")
+        parser.add_argument("password", type=str, required=True, help="password required")
         parser.add_argument("name", type=str, required=True, help="item name required")
+        parser.add_argument("sell_in", type=str, required=True, help="item sell_in required")
+        parser.add_argument("quality", type=str, required=True, help="item quality required")
         return parser.parse_args()
